08ca968b299f65b8c7aba5d81ff3316a9ff5f287
maynardsmith <tony.maynard-smith@ntlworld.com>
2022-04-01 17:13:25 +0300

    SQL add Views for "latest" reports

---
+++ sql/views/ViewsContacts.sql
@@ -0,0 +1,26 @@
+/* Create Views to aggregate or filter Tables for Contacts Statistics */
+
+-- Select Household Sizes for Latest Month
+CREATE OR REPLACE VIEW lalg_stats_view_latest_sizes AS
+
+SELECT a.*
+FROM lalg_stats_household_sizes AS a
+JOIN ( 
+       SELECT MAX(Sample_Date) AS maxd
+       FROM lalg_stats_household_sizes
+     ) AS b 
+  ON a.Sample_date = b.maxd
+  
+  
+-- Select Household Postcodes for Latest Month
+CREATE OR REPLACE VIEW lalg_stats_view_latest_postcodes AS
+
+SELECT a.*
+FROM lalg_stats_household_postcodes AS a
+JOIN ( 
+       SELECT MAX(Sample_Date) AS maxd
+       FROM lalg_stats_household_postcodes
+     ) AS b 
+  ON a.Sample_date = b.maxd  
+  
+  
